11dee7b6cf269aaa4972d1cc1f3f1339cd38d85b
Marina Glancy <marina@moodle.com>
2021-09-15 12:02:32 +0200

    Version bump 3.11.3

--- version.php
+++ version.php
@@ -37,10 +37,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'block_mylearning';
-$plugin->release      = '3.11.2';
-$plugin->version      = 2021073000;
-$plugin->requires     = 2021051702.00;
+$plugin->release      = '3.11.3';
+$plugin->version      = 2021091600;
+$plugin->requires     = 2021051703.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_program' => 2021073000,
+    'tool_program' => 2021091600,
 ];
