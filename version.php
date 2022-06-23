0ce18f8e60fccabc53371bae521c7788c53e6447
Marina Glancy <marina@moodle.com>
2022-06-23 10:20:41 +0200

    WP-3428 version bump 2022062300

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'block_mylearning';
 $plugin->release      = '4.0dev';
-$plugin->version      = 2022042500;
+$plugin->version      = 2022062300;
 $plugin->requires     = 2022041900.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
