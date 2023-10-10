1686639462c80050a63138d9e74c8ef042e1c5b0
Marina Glancy <marina@moodle.com>
2023-10-10 12:48:25 +0100

    Version bump 4.2.3

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'block_mylearning';
-$plugin->release      = '4.2';
-$plugin->version      = 2023053000;
+$plugin->release      = '4.2.3';
+$plugin->version      = 2023101000;
 $plugin->requires     = 2022041904.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
     'tool_program' => 2022111500,
 ];
-$plugin->supported    = [400, 401];
+$plugin->supported    = [400, 402];
